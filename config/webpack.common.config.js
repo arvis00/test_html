2f976808e884f9eaeacc9c13dc945c5f01a8e0a4
Arvydas_Mickus <amickus@icloud.com>
2019-10-24 19:10:34 +0300

    first commit

---
+++ config/webpack.common.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    // devServer: {
+    //     contentBase: '../dist',
+    // },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    // watch:true,
+    module: {
+        rules: [{
+                test: [/.js$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+            {
+                test: [/.css$|.scss$/],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    "postcss-loader",
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets/images'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [{
+                  loader: "file-loader",
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: 'assets/fonts'
+                  }
+                }]
+              }
+            
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Html_test',
+            template: './src/index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[chunkhash].css'
+        }),
+
+        new CopyWebpackPlugin([{
+                from: './src/assets/images',
+                to: 'assets/images'
+            },
+            {
+                from: './src/assets/icons',
+                to: 'assets/icons'
+            }
+        ]),
+        new CleanWebpackPlugin()
+    ]
+};
---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+
+import '../src/style.css';
+import './styles/scss/main.scss';
+import './app/code';
+
+
+// import './assets/images/climb.jpg';
+// import loginpng from './assets/images/login.png';
+// import cloud_2png from './assets/images/cloud_2.png';
+// import portraitjpg from './assets/images/portrait.jpg';
+// import facebookpng from './assets/images/facebook.png';
+// import twitterpng from './assets/images/twitter.png';
+// import instagrampng from './assets/images/instagram.png';
+// import youtubepng from './assets/images/youtube.png';
+// import attachmentsvg from './assets/icons/attachment.svg';
+// import closesvg from './assets/icons/close.svg';
+// import emojisvg from './assets/icons/emoji.svg';
+// import sendsvg from './assets/icons/send.svg';
+// import up_arrowsvg from './assets/icons/up_arrow.svg';
+
+// document.getElementById('climb-jpg').setAttribute('src', climbjpg);
